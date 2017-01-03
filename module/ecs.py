ccc9823a7689fdb189dc535d8e407dd9be231a38
xiaozhu36 <guimin.hgm@alibaba-inc.com>
2017-01-03 10:29:03 +0800

    move DOCUMENTATION into ecs.py

--- module/ecs.py
+++ module/ecs.py
@@ -1,3 +1,287 @@
+DOCUMENTATION = '''
+---
+module: ecs
+short_description: create, start, stop, restart or terminate an instance in ecs
+common options:
+  acs_access_key:
+    description: The access key.
+    required: false
+    default: null
+    aliases: []
+  acs_secret_access_key:
+    description: The access secret key.
+    required: false
+    default: null
+    aliases: []
+  state:
+    description: The state of the instance after operating.
+    required: true
+    default: null
+    aliases: []
+    choices: ['pending', 'running', 'stopped', 'restarted', 'absent'] map operation ['create', 'start', 'stop', 'restart', 'terminate']
+
+function: create instance
+  description: create an instance in ecs
+  options:
+    region:
+      description: The Aliyun region ID to use for the instance.
+      required: true
+      default: null
+      aliases: [ 'acs_region', 'ecs_region' ]
+    zone_id:
+      description: Aliyun availability zone ID in which to launch the instance
+      required: false
+      default: null
+      aliases: [ 'acs_zone', 'ecs_zone' ]
+    image_id:
+      description: Image ID to use for the instance.
+      required: true
+      default: null
+      aliases: []
+    instance_type:
+      description: Instance type to use for the instance
+      required: true
+      default: null
+      aliases: [ 'type' ]
+    group_id:
+      description: Security group id to use with the instance
+      required: false
+      default: null
+      aliases: []
+    io_optimized:
+      description: Whether instance is using optimized volumes.
+      required: false
+      default: False
+      aliases: []
+    vswitch_id:
+      description: The subnet ID in which to launch the instance (VPC).
+      required: false
+      default: null
+      aliases: []
+    instance_name:
+      description: Name of the instance to use.
+      required: false
+      default: null
+      aliases: []
+    description:
+      description: Descripton of the instance to use.
+      required: false
+      default: null
+      aliases: []
+    internet_data:
+      description:
+        - A hash/dictionaries of internet to the new instance;
+        - '{"key":"value"}'; keys allowed:
+          - charge_type (required:false; default: "PayByBandwidth", choices:["PayByBandwidth", "PayByTraffic"] )
+          - max_bandwidth_in(required:false, default:200)
+          - max_bandwidth_out(required:false, default:0).
+      required: false
+      default: null
+      aliases: []
+    host_name:
+      description: Instance host name.
+      required: false
+      default: null
+      aliases: []
+    password:
+      description: The password to login instance.
+      required: false
+      default: null
+      aliases: []
+    system_disk:
+      description:
+        - A hash/dictionaries of system disk to the new instance;
+        - '{"key":"value"}'; keys allowed:
+          - disk_category (required:false; default: "cloud"; choices:["cloud", "cloud_efficiency", "cloud_ssd", "ephemeral_ssd"] )
+          - disk_size (required:false; default:max{40，ImageSize}; choices:[40~500])
+          - disk_name (required:false; default:null)
+          - disk_description (required:false; default:null)
+      required: false
+      default: null
+      aliases: []
+    volumes:
+      description:
+        - A list of hash/dictionaries of volumes to add to the new instance;
+        - '[{"key":"value", "key":"value"}]'; keys allowed:
+          - device_category (required:false; default: "cloud"; choices:["cloud", "cloud_efficiency", "cloud_ssd", "ephemeral_ssd"] )
+          - device_size (required:false; default:null; choices:depends on disk_category)
+          - device_name (required: false; default:null)
+          - device_description (required: false; default:null)
+          - delete_on_termination (required:false, default:"true")
+          - snapshot (required:false; default:null), volume_type (str), iops (int) - device_type is deprecated use volume_type, iops must be set when volume_type='io1', ephemeral and snapshot are mutually exclusive.
+      required: false
+      default: null
+      aliases: []
+    count:
+      description: The number of the new instance.
+      required: false
+      default: 1
+      aliases: []
+    allocate_public_ip
+      description: Whether allocate a public ip for the new instance.
+      required: false
+      default: true
+      aliases: []
+    bind_eip:
+      description: ID of Elastic IP Address bind to the new instance.
+      required:false
+      default: null
+      aliases: []
+    instance_tags:
+      description: - A list of hash/dictionaries of instance tags, '[{tag_key:"value", tag_value:"value"}]', tag_key must be not null when tag_value isn't null
+      required: false
+      default: null
+      aliases: []
+    ids:
+      description:
+        - A list of identifier for this instance or set of instances, so that the module will be idempotent with respect to ECS instances. This identifier should not be reused for another call later on. For details, see the description of client token at U(https://help.aliyun.com/document_detail/25693.html?spm=5176.doc25499.2.7.mrVgE2).
+        - The length of the ids is the same with count
+      required: false
+      default: null
+    instance_charge_type:
+      description: - The charge type of the instance.
+      required: false
+      choices:["PrePaid", "PostPaid"]
+      default: "PostPaid"
+    period:
+      description: - The charge duration of the instance, the value is vaild when instance_charge_type is "PrePaid".
+      required: false
+      choices:[1~9,12,24,36]
+      default: null
+    auto_renew:
+      description: - Whether automate renew the charge of the instance.
+      required: false
+      choices:[true, false]
+      default: false
+    auto_renew_period:
+      description: - The duration of the automatic renew the charge of the instance. It is vaild when auto_renew is true.
+      required: false
+      choices:[1, 2, 3, 6, 12]
+      default: false
+
+
+function: start, stop, restart, terminate instance
+  description: start, stop, restart and terminate instancesin ecs
+  options:
+    instance_ids:
+      description: A list of instance ids, currently used for states: running, stopped, restarted, absent
+      required: true
+      default: null
+      aliases: []
+    force:
+      description: Whether force to operation, currently used fo states: stopped, restarted.
+      required: false
+      default: False
+      aliases: []
+    instance_tags:
+      description: - A list of hash/dictionaries of instance tags, '[{tag_key:"value", tag_value:"value"}]', tag_key must be not null when tag_value isn't null
+      required: false
+      default: null
+      aliases: []
+
+function: modify instance attribute
+  description: modufy instances attributes in ecs
+  options:
+    attributes:
+      description:
+        - A list of hash/dictionaries of instance attributes
+        - '[{"key":"value", "key":"value"}]', keys allowed:
+          - id (required:true; default: null )
+          - name (required:false)
+          - description (required:false)
+          - password (required:false)
+          - host_name (required:false)
+          - vnc_password (required:false)
+      required: false
+      default: null
+      aliases: []
+
+function: modify instance vpc attribute
+  description: modufy instances attributes in ecs
+  options:
+    vpc_attributes:
+      description:
+        - A list of hash/dictionaries of instance vpc attributes
+        - '[{"key":"value", "key":"value"}]', keys allowed:
+          - instance_id (required:true; default: null )
+          - vswitch_id (required:false)
+          - eip (required)
+      required: false
+      default: null
+      aliases: []
+
+function: modify instance security group attribute
+  description: join or leave instances from security group.
+  options:
+    group_id:
+      description: Security group id (or list of ids) to use with the instance
+      required: true
+      default: null
+      aliases: []
+    instance_ids:
+      description: A list of instance ids.
+      required: true
+      default: null
+      aliases: []
+    sg_action:
+      description: The action of operating security group.
+      required: true
+      default: null
+      choices: ['join','remove']
+      aliases: []
+'''
+
+EXAMPLES = '''
+- action: modulename opt1=arg1 opt2=arg2
+
+## start or terminate instance
+- name: start or terminate instance
+  hosts: localhost
+  vars:
+    acs_access_key: XXXXXXXXXXXXXX
+    acs_secret_access_key: XXXXXXXXXXXXXX
+    region: cn-shenzhen
+    instance_ids: i-94dehop6n
+    instance_tags:
+    - tag_key: xz_test
+      tag_value: '1.20'
+    state: running
+  tasks:
+    - name: start instance
+      ecs_model:
+        acs_access_key: '{{ acs_access_key }}'
+        acs_secret_access_key: '{{ acs_secret_access_key }}'
+        region: '{{ region }}'
+        instance_ids: '{{ instance_ids }}'
+        instance_tags: '{{ instance_tags }}'
+        state: '{{ state }}'
+
+## stop or restarted instance
+- name: start stop restart instance
+  hosts: localhost
+  vars:
+    acs_access_key: XXXXXXXXXXXXXX
+    acs_secret_access_key: XXXXXXXXXXXXXX
+    region: cn-shenzhen
+    instance_ids: i-94dehop6n
+    instance_tags:
+    - tag_key: xz_test
+      tag_value: '1.20'
+    force: False
+    state: restarted
+  tasks:
+    - name: start instance
+      ecs_model:
+        acs_access_key: '{{ acs_access_key }}'
+        acs_secret_access_key: '{{ acs_secret_access_key }}'
+        region: '{{ region }}'
+        instance_ids: '{{ instance_ids }}'
+        instance_tags: '{{ instance_tags }}'
+        state: '{{ state }}'
+
+'''
+
+
 import time
 from ast import literal_eval
 
