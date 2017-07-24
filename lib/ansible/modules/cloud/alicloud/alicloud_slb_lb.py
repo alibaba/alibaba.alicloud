a0c0e1daac070cbb8ab7a41afaab3b8c2ba8bb4d
wb-lq277737 <wb-lq277737@alibaba-inc.com>
2017-07-24 00:13:26 -0700

    delete useless spaces

--- lib/ansible/modules/cloud/alicloud/alicloud_slb_lb.py
+++ lib/ansible/modules/cloud/alicloud/alicloud_slb_lb.py
@@ -294,22 +294,19 @@ def main():
         bandwidth = module.params['bandwidth']
         res_objs = []
         changed = False
-        result = []
+        cur_slb = None
         
         if load_balancer_id and load_balancer_name:
             name_test = ""
         else:
             name_test = load_balancer_name
         res_objs = get_load_balancer_objs(slb, load_balancer_id = load_balancer_id, load_balancer_name = name_test)
-            
-        cur_slb = None
-        
         if len(res_objs)==1:
-            cur_slb = res_objs[0]            
+            cur_slb = res_objs[0]
 
         if state == "absent" and cur_slb:
             changed = cur_slb.delete()
-            module.exit_json(changed=changed, load_balancer_id=cur_slb.load_balancer_id)    
+            module.exit_json(changed=changed, load_balancer_id=cur_slb.load_balancer_id)
         elif state == "present":
             if load_balancer_status and cur_slb:
                 #set status
@@ -321,14 +318,14 @@ def main():
                 #set name  
                 changed = cur_slb.modify_name(load_balancer_name)
                 if changed:
-                    cur_slb.load_balancer_name = load_balancer_name 
+                    cur_slb.load_balancer_name = load_balancer_name
                 module.exit_json(changed=changed, load_balancer = get_info(cur_slb), load_balancer_id = cur_slb.load_balancer_id) 
             elif (internet_charge_type or bandwidth) and cur_slb:
                 #set spec
                 changed = cur_slb.modify_spec(internet_charge_type=internet_charge_type, bandwidth=bandwidth)
                 if changed:
                     cur_slb.internet_charge_type = internet_charge_type
-                    cur_slb.bandwidth = bandwidth    
+                    cur_slb.bandwidth = bandwidth
                 module.exit_json(changed=changed, load_balancer = get_info(cur_slb), load_balancer_id = cur_slb.load_balancer_id) 
             elif not cur_slb:
                 res_obj = slb.create_load_balancer(load_balancer_name=load_balancer_name,
@@ -344,4 +341,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-    
