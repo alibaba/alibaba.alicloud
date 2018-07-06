ad597b28e5a746ab68b7842c692f688a14378823
He Guimin <guimin.hgm@alibaba-inc.com>
2018-07-06 16:47:28 +0800

    Correct module docs

--- lib/ansible/modules/cloud/alicloud/ali_vswitch_facts.py
+++ lib/ansible/modules/cloud/alicloud/ali_vswitch_facts.py
@@ -26,7 +26,7 @@ ANSIBLE_METADATA = {'metadata_version': '1.1',
 
 DOCUMENTATION = '''
 ---
-module: alicloud_vswitch_facts
+module: ali_vswitch_facts
 version_added: "1.5.0"
 short_description: Gather facts on vswitchs of Alibaba Cloud.
 description:
