bcab221c6121189e0ed81fe71a0f9d17d32dfb37
Yang Liu <liuyang@bit.fish>
2019-12-13 10:53:11 +0800

    change back python version

--- contrib/inventory/alicloud.py
+++ contrib/inventory/alicloud.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 
 # Copyright (c) 2017-present Alibaba Group Holding Limited. He Guimin <heguimin36@163.com>
 # GNU General Public License v3.0+ (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
