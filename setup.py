8dc527a4a88d8eee0c82a90be0176ad5c82db231
lixue <1120722567@qq.com>
2020-02-28 10:41:43 +0800

    modify footmark version

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ AUTHOR = "xiaozhu"
 AUTHOR_EMAIL = "heguimin36@163.com"
 URL = "https://github.com/alibaba/ansible-provider/tree/master/lib/ansible"
 
-VERSION = "1.16.0"
+VERSION = "1.16.1"
 
 setup(
     name=NAME,
@@ -59,5 +59,5 @@ setup(
     package_dir={'': 'lib'},
     packages=find_packages('lib'),
     include_package_data=True,
-    install_requires=['ansible', 'footmark>=1.17.0', 'ansible_alicloud_module_utils>=1.3.0']
+    install_requires=['ansible', 'footmark>=1.17.1', 'ansible_alicloud_module_utils>=1.3.0']
 )
